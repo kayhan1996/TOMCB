c5df884344822d2c842e5b011b6c073062faa35d
kayhan1996 <kayhan1996@gmail.com>
2018-01-19 06:43:06 -0700

    Manually fixed file

--- TOMCB/ViewController.swift
+++ TOMCB/ViewController.swift
@@ -7,14 +7,88 @@
 //
 
 import UIKit
+import CoreBluetooth
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CBCentralManagerDelegate, UITableViewDataSource {
+    
+    let UUID = CBUUID(string: "0x180D")
+    var centralManager: CBCentralManager!;
+    var mainPeripheral: CBPeripheral!
+    var peripherals = [CBPeripheral]()
+    var data = [String]()
+    
+    @IBOutlet weak var table: UITableView!
+    @IBOutlet weak var bluetoothStatus: UILabel!
+    @IBOutlet weak var deviceStatus: UILabel!
+    @IBOutlet weak var scanIndicator: UILabel!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        switch central.state {
+        case .unknown:
+            fatalError("Unknown Error")
+        case .resetting:
+            fatalError("Unknown Error")
+        case .unsupported:
+            fatalError("Unknown Error")
+        case .unauthorized:
+            fatalError("Unknown Error")
+        case .poweredOff:
+            print("Bluetooth OFF!")
+            scanIndicator.text = "Bluetooth OFF"
+        case .poweredOn:
+            bluetoothStatus.text = "On";
+            scanIndicator.text = "Scanning for devices"
+            activityIndicator.startAnimating()
+            centralManager.scanForPeripherals(withServices: nil);
+        }
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        peripherals.append(peripheral);
+        data.append(peripheral.name!)
+        table.reloadData()
+        scanIndicator.text = "devices found"
+    }
+    
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        print("Connected")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        bluetoothStatus.text = "OFF"
+        deviceStatus.text = "Disconnected"
+        table.dataSource = self;
     }
-
+    
+    //MARK: Table
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1;
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "Devices") as UITableViewCell! else {
+            fatalError("Unable to create tables")
+        }
+        let text = data[indexPath.row]
+        cell.textLabel?.text = text;
+        return cell;
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
+        mainPeripheral = peripherals[indexPath.row];
+        centralManager.stopScan()
+        scanIndicator.text = "Connecting"
+        centralManager.connect(mainPeripheral)
+        deviceStatus.text = "Connected"
+        activityIndicator.stopAnimating();
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -22,4 +96,5 @@ class ViewController: UIViewController {
 
 
 }
+
 
